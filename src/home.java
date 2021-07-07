97fc4b5c1a9e71798e4b88ca4fbd391a6d0b2307
Camilo Molano <ecamilo.molano@gmail.com>
2021-07-07 18:59:55 -0500

    puse un hola camilo

--- src/home.java
+++ src/home.java
@@ -2,7 +2,7 @@ public class home {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        System.out.println();
+        System.out.println("Hola camilo");
         System.out.println();
     }
 }
